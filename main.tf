f2365272d46a166011f00b2f91e4734d0c4412de
jaysson1710 <jaysson1710@hotmail.com>
2022-08-25 11:39:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,11 +8,19 @@ terraform {
     }
   }
 }
+provider "azurerm" {
+  features {
+    
+  }
+}
 
+data "azurerm_resource_group" "rg" {
+  name = var.rg
+}
 
 resource "azurerm_storage_account" "storage" {
   name                     = var.nombre_storage
-  resource_group_name      = var.rg.name
+  resource_group_name      = data.azurerm_resource_group.rg.name
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
