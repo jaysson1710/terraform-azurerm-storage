dc4ef4b54d4b4ae12019947834720034b49a9c8e
jaysson1710 <jaysson1710@hotmail.com>
2022-09-12 17:29:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 
 resource "azurerm_storage_account" "storage" {
   name                     = var.nombre_storage
-  resource_group_name      = data.azurerm_resource_group.rg.name
+  resource_group_name      = var.rg
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
